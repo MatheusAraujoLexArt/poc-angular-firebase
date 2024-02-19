4dcdc4014be03c98aa0b793fe9be918672476efb
Matheus <matheus.araujo@lexartlabs.xyz>
2024-02-19 15:43:21 -0300

    Add: setup firebase

---
+++ poc-angular-firebase/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ poc-angular-firebase/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAqtlYuPwOTHxVSjt0bDwPJNYannThDuN4",
+    authDomain: "poc-angular-firebase-af084.firebaseapp.com",
+    projectId: "poc-angular-firebase-af084",
+    storageBucket: "poc-angular-firebase-af084.appspot.com",
+    messagingSenderId: "319955431628",
+    appId: "1:319955431628:web:310a4ab7dd389fe1705269"
+  }
+};
