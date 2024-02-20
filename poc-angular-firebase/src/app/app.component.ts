492bea121623132228d8fde83653dd478b402199
Matheus <matheus.araujo@lexartlabs.xyz>
2024-02-20 19:14:57 -0300

    Fix: component warning errors

--- poc-angular-firebase/src/app/app.component.ts
+++ poc-angular-firebase/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AccountFormComponent } from './components/account-form/account-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AccountFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- poc-angular-firebase/src/app/components/account-form/account-form.component.ts
+++ poc-angular-firebase/src/app/components/account-form/account-form.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-form',
   standalone: true,
+  imports: [],
   templateUrl: './account-form.component.html',
-  styleUrls: ['./account-form.component.css'] // Alteração para styleUrls e fornecimento de um array
+  styleUrl: './account-form.component.css'
 })
-export class AccountFormComponent {
+
+// export class AccountFormComponent {
+//  // se der algum erro, voltar com essa classe
+// }
+
+export class AccountFormComponent implements OnInit  {
+  userForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get email() {
+    return this.userForm.get('email')!;
+  }
+
+  get password() {
+    return this.userForm.get('password')!;
+  }
+
   title = 'teste';
+  submit() {
+    console.log('Enviou formulario')
+  }
 }
