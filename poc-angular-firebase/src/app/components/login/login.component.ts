4810958059a8c8f132c22f8baaf968c4083a4bcb
Matheus <matheus.araujo@lexartlabs.xyz>
2024-02-21 15:55:55 -0300

    Add: form validations

--- poc-angular-firebase/src/app/components/login/login.component.ts
+++ poc-angular-firebase/src/app/components/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  login() {
+    if(!this.loginForm.valid) {
+      return;
+    }
+
+    console.log('Validations passed!')
+  }
+
+  email = this.loginForm.get('email');
+  password = this.loginForm.get('password');
 }
