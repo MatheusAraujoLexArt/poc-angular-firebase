0bec8d8035d640f31cddf05c0fd50c136dc1024e
Matheus <matheus.araujo@lexartlabs.xyz>
2024-02-23 16:42:52 -0300

    Feat: Create, Delete and Fetch todos

--- poc-angular-firebase/src/app/components/home/home.component.ts
+++ poc-angular-firebase/src/app/components/home/home.component.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { CommonModule } from '@angular/common';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../services/auth.service';
+import { MatTableModule } from '@angular/material/table';
+import { ITodo } from '../../models/todo';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  fb = inject(NonNullableFormBuilder);
+  usersService = inject(UsersService);
+  authService = inject(AuthService);
+  currentUser = this.authService.currentUser$;
 
+  todoForm = this.fb.group({
+    description: ['', Validators.required],
+  })
+
+  todos: any = [];
+  // editingTodoId: string | null = null;
+  displayedColumns: string[] = ['createdAt', 'description', 'actions'];
+
+  async ngOnInit(): Promise<void> {
+    const userId = this.authService.firebaseAuth.currentUser?.uid;
+
+    if (userId) {
+      this.todos = await this.usersService.getTodosByUserId(userId);
+    }
+  }
+
+  async deleteTodo(todoId: string): Promise<void> {
+    await this.usersService.deleteTodoByUserId(todoId)
+    .then(async () => {
+      const userId = this.authService.firebaseAuth.currentUser?.uid;
+
+      if (userId) {
+        this.todos = await this.usersService.getTodosByUserId(userId);
+      }
+    })
+  }
+
+  async createTodo(): Promise<void> {
+    const userId = this.authService.firebaseAuth.currentUser?.uid;
+    const { description } = this.todoForm.value;
+    
+    if(userId && description) {
+      await this.usersService.createTodo({
+        userId: userId,
+        description: description,
+        createdAt: new Date().toISOString(), 
+      })
+      .then(async () => {
+        // this.todoForm.reset();
+        const userId = this.authService.firebaseAuth.currentUser?.uid;
+        if (userId) {
+          this.todos = await this.usersService.getTodosByUserId(userId);
+        }
+      })
+    }
+  }
+
+  formatDate(isoDateString: string): string {
+    return new Date(isoDateString).toLocaleDateString("pt-BR");
+  }
+
+  // editTodo(todoId: string): void {
+  //   this.editingTodoId = todoId;
+  // }
+
+  // cancelEdit(): void {
+  //   this.editingTodoId = null;
+  // }
+
+  // async updateTodo(todoId: string): Promise<void> {
+  //   const userId = this.authService.firebaseAuth.currentUser?.uid;
+  //   const { description } = this.todoForm.value;
+  //   // const todoId = this.editingTodoId;
+  //   if(!userId) {
+  //     console.log('sem userId')
+  //   }
+  //   if(!description) {
+  //     console.log('sem userId')
+  //   }
+  //   if(!userId) {
+  //     console.log('sem userId')
+  //   }
+  //   if (userId && description && todoId) {
+  //     console.log('entrou')
+  //     await this.usersService.updateTodo({
+  //       description: description,
+  //       id: todoId,
+  //     })
+  //     .then(async () => {
+  //       // this.todoForm.reset();
+  //       const userId = this.authService.firebaseAuth.currentUser?.uid;
+  //       if (userId) {
+  //         this.todos = await this.usersService.getTodosByUserId(userId);
+  //       }
+  //     })
+
+  //     // Após a atualização, saia do modo de edição
+  //     this.editingTodoId = null;
+  //   }
+  // }
+
+  description = this.todoForm.get('description');
 }
--- poc-angular-firebase/src/app/services/users.service.ts
+++ poc-angular-firebase/src/app/services/users.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, doc, docData, setDoc, updateDoc } from '@angular/fire/firestore';
+import { Firestore, collection, deleteDoc, setDoc, doc, docData, getDocs, query, updateDoc, where } from '@angular/fire/firestore';
 import { ProfileUser } from '../models/user';
 import { AuthService } from './auth.service';
 import { Observable, of, switchMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { ITodo } from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
@@ -37,4 +38,39 @@ export class UsersService {
     const ref = doc(this.firestore, 'users', user.uid);
     return updateDoc(ref, { ...user });
   }
+
+  async getTodosByUserId(userId: string) {
+    const todosArray: any = [];
+    const q = query(
+      collection(this.firestore, "todos"),
+      where("userId", "==", userId)
+    );
+
+    const userSnapshot = await getDocs(q);
+    userSnapshot.forEach((doc) => {
+      if (doc.data()) {
+        todosArray.push({
+          ...doc.data(),
+          id: doc.id,
+        })
+      }
+    });
+
+    return todosArray;
+  }
+
+  async deleteTodoByUserId(todoId: string) {
+    const todoRef = doc(this.firestore, 'todos', todoId);
+    await deleteDoc(todoRef);
+  }
+
+  async createTodo(newTodo: {userId: string, description: string, createdAt: string}) {
+    const ref = doc(collection(this.firestore, "todos"));
+    await setDoc(ref, newTodo);
+  }
+
+  // async updateTodo({ description, id } : { id :string, description: string }) {
+  //   const ref = doc(collection(this.firestore, "todos", id));
+  //   await updateDoc(ref, { description, id });
+  // }
 }
